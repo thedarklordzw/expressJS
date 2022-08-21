408d4182273cac569031888c9490fa332dbf340f
Ulathi <swiftly.graphics@gmail.com>
2022-08-21 12:43:19 +0200

    express v1.01

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/api');
+const cors = require('cors');
 const PORT = 8080;
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
